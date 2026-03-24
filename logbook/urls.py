65c7c02cc8924ac51692bac308b6cfc90f79e905
jerez <jerezamaerodrigo1018@gmail.com>
2026-03-24 17:11:07 +0800

    aa

--- logbook/urls.py
+++ logbook/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/registration/login/', permanent=True)),
+    path('', RedirectView.as_view(url='/logbook/login/', permanent=True)),
     path('logbook/', include('registration.urls')),
 ]
 
