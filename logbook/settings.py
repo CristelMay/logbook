b6c59c460c2bd40e65852082d95343d16cf9c118
jerez <jerezamaerodrigo1018@gmail.com>
2026-04-01 14:40:45 +0800

    security settings

--- logbook/settings.py
+++ logbook/settings.py
@@ -57,6 +57,21 @@ else:
     print(f"ENV value: Inside Development Block {ENV}")
     ALLOWED_HOSTS = ['*']
 
+if ENV == 'production':
+    DEBUG = False
+    # ... your existing code ...
+
+    # Security headers
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
+
 
 # Application definition
 
